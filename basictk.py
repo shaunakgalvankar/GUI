496a22d15c2a5943ae8cbc11b178930da599ce76
Sshaonak P Galvankar <shaunakprasad@icloud.com>
2018-05-05 14:40:12 +0530

    Add files via upload

---
+++ basictk.py
@@ -0,0 +1,9 @@
+#this is one from the series of learning making GUI's using tkinter
+import tkinter 
+
+root=tkinter.Tk()
+#intitiating the tkinter object
+
+root.mainloop()
+#this keeps the GUI running till we click the quit button
+
